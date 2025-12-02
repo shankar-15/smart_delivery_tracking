672157625a74b5069782a8909204c15ead422488
shreya <shreyashankar.bko@gmail.com>
2025-12-02 23:53:42 +0530

    checking1

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/cupertino.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({super.key});
+
+  @override
+  State<splashscreen> createState() => _splashscreenstate();
+}
+class _splashscreenstate extends State<splashscreen>{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+
+    //
+  }
+
+}
