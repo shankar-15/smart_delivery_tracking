1af47257dbd3a5b489630a0b12a024dc3b43d57c
shreya <shreyashankar.bko@gmail.com>
2025-12-08 23:20:12 +0530

    day2, bottom navigation

---
+++ lib/screens/bottomnavigation.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:sos_application/screens/profilescreen.dart';
+import 'package:sos_application/screens/trackorder.dart';
+
+import 'homescreen.dart';
+
+
+class MainHomeScreen extends StatefulWidget {
+  const MainHomeScreen({super.key});
+
+  @override
+  State<MainHomeScreen> createState() => _MainHomeScreenState();
+}
+
+class _MainHomeScreenState extends State<MainHomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeTab(),
+    OrdersTab(),
+    ProfileTab(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: const Color(0xFF1976D2),
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inbox_outlined),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
